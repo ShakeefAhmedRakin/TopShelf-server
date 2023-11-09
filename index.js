c319f4f7352bbf5250f13c94b3debcb5378b7b55
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2023-11-09 14:53:04 +0600

    Updated the delete cookie and redeployed to vercel.

--- index.js
+++ index.js
@@ -83,7 +83,13 @@ async function run() {
     app.post("/logout", async (req, res) => {
       const user = req.body;
       console.log("Logging Out", user);
-      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+      res
+        .clearCookie("token", {
+          maxAge: 0,
+          secure: process.env.NODE_ENV === "production" ? true : false,
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
     });
 
     // CATEGORY API
